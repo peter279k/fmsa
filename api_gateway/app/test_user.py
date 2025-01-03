a0509f4532ff7e1534a398ca8e5c815b8af54f93
peter279k <peter279k@gmail.com>
2025-01-03 19:12:38 +0800

    Complete user feature test (register and login)

---
+++ api_gateway/app/test_user.py
@@ -0,0 +1,43 @@
+import os
+from .main import app
+from fastapi.testclient import TestClient
+
+
+client = TestClient(app)
+
+def test_get_fsma_version():
+    response = client.get('/api/v1/')
+
+    assert response.status_code == 200
+    assert response.json()['message'] == 'v1'
+
+def test_register_user():
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    data = {
+        'username': 'test',
+        'password': 'test123',
+    }
+    response = client.post('/api/v1/register', headers=headers, json=data)
+
+    assert response.status_code == 200
+
+def test_duplicated_register_user():
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    data = {
+        'username': 'test_duplicated',
+        'password': 'test123',
+    }
+    response = client.post('/api/v1/register', headers=headers, json=data)
+
+    assert response.status_code == 200
+
+    duplicated_response = client.post('/api/v1/register', headers=headers, json=data)
+    duplicated_json_response = duplicated_response.json()
+
+    assert duplicated_response.status_code == 200
+    assert len(duplicated_json_response['data']) > 0
+    assert duplicated_json_response['status'] == 409
