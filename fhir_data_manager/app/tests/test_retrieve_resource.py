db2e23deed5db912ac28a08757e85687459c52c5
peter279k <peter279k@gmail.com>
2025-01-15 22:54:36 +0800

    Add retrieve_patient_resource with_query_params

--- fhir_data_manager/app/tests/test_retrieve_resource.py
+++ fhir_data_manager/app/tests/test_retrieve_resource.py
@@ -12,3 +12,13 @@ def test_retrieve_patient_resource_without_query_params():
 
     assert response.status_code == 200
     assert response.json()['message'] == 'Retrieving Patient is successful.'
+
+def test_retrieve_patient_resource_not_found_with_query_params():
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+
+    response = client.get('/api/v1/retrieve/Patient?_id=not_found', headers=headers)
+    response_json = response.json()
+
+    assert response.status_code == 200
+    assert response_json['message'] == 'Retrieving Patient is successful.'
+    assert response_json['data'][0]['total'] == 0
