c1a05afe67f11a873561334841b72852ebbfbe81
peter279k <peter279k@gmail.com>
2025-01-30 01:53:41 +0800

    Add Encounter-min for IMRI feature and test

--- fhir_generator/app/modules/Track8ForEncounter.py
+++ fhir_generator/app/modules/Track8ForEncounter.py
@@ -1,5 +1,6 @@
 import hashlib
 import secrets
+from fhir_data_generator import EncounterImri
 from fhir_data_generator import TWCoreEncounter as Encounter
 
 
@@ -33,6 +34,31 @@ class Track8ForEncounter:
         encounter_class.set_period(self.encounter_payload['period'])
 
         encounter_class.set_length(self.encounter_payload['length'])
+
+        encounter_class.create()
+
+        return encounter_class.payload_template
+
+    def generate_encounter_min_resource(self):
+        resource_id = hashlib.sha3_224(secrets.token_urlsafe(5).encode('utf-8')).hexdigest()
+        encounter_class = EncounterImri(resource_id)
+
+        profile_urls = self.encounter_payload['profile_urls']
+        encounter_class.set_profile_urls(profile_urls)
+
+        encounter_class.set_status(self.encounter_payload['status'])
+
+        encounter_class.set_class(self.encounter_payload['fixture_class'])
+
+        encounter_class.set_service_type_coding(self.encounter_payload['service_type_coding'])
+
+        encounter_class.set_subject(self.encounter_payload['subject'])
+
+        encounter_class.set_participant_individual(self.encounter_payload['participant_individual'])
+
+        encounter_class.set_period(self.encounter_payload['period'])
+
+        encounter_class.set_hospitalization(self.encounter_payload['hospitalization'])
 
         encounter_class.create()
 
--- fhir_generator/app/routers/encounter.py
+++ fhir_generator/app/routers/encounter.py
@@ -1,18 +1,23 @@
 from app.item_models.track8_2024 import *
 from app.modules import Track8ForEncounter
 
+from fastapi.requests import Request
 from fastapi.responses import JSONResponse
 
 
-async def generate_track8_2024_for_encounter(item: Track8ForResource):
+async def generate_track8_2024_for_encounter(request: Request, item: Track8ForResource):
     status_code = 200
     resource_name = 'Encounter'
     item_dict = item.model_dump()
     encounter_resource = {}
+    encounter_type = request.query_params.get('type', '')
 
     try:
         track8_for_encounter = Track8ForEncounter.Track8ForEncounter(resource_name, item_dict)
-        encounter_resource = track8_for_encounter.generate_encounter_resource()
+        if encounter_type == 'imri-min':
+            encounter_resource = track8_for_encounter.generate_encounter_min_resource()
+        else:
+            encounter_resource = track8_for_encounter.generate_encounter_resource()
     except Exception as e:
         status_code = 500
 
