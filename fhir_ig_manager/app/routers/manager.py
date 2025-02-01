62122a279dcbd4560fa03c6c223bfdfeaf444a9f
peter279k <peter279k@gmail.com>
2025-02-01 16:48:53 +0800

    Change item_dict item.model_dump position

--- fhir_ig_manager/app/routers/manager.py
+++ fhir_ig_manager/app/routers/manager.py
@@ -53,9 +53,9 @@ async def retrieve_ig(request: Request):
 
 async def create_ig_metadata(item: ImplementationGuideMetadata):
     status_code = 200
+    item_dict = item.model_dump()
     try:
         ig_manager = ImplementationGuideManager.ImplementationGuideManager()
-        item_dict = item.model_dump()
         ig_manager.create_metadata(item_dict)
     except Exception as e:
         status_code = 500
