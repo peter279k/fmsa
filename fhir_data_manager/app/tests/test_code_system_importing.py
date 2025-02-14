df2380aec123fc1a4bd834eb3a49e02192ef2dda
peter279k <peter279k@gmail.com>
2025-02-15 01:18:04 +0800

    Add test_retrieve_code_system_log test

--- fhir_data_manager/app/tests/test_code_system_importing.py
+++ fhir_data_manager/app/tests/test_code_system_importing.py
@@ -1,3 +1,5 @@
+import time
+import pytest
 import subprocess
 from app.main import app
 from urllib.parse import urlencode
@@ -7,6 +9,7 @@ from fastapi.testclient import TestClient
 processed_id = ''
 client = TestClient(app)
 
+@pytest.mark.dependency()
 def test_import_archived_code_system():
     filename = {
         'filename': 'Loinc_2.72.zip'
@@ -25,8 +28,24 @@ def test_import_archived_code_system():
 
     assert response.status_code == 200
     assert response_json['message'] == 'Importing the {} is on the way.'.format(filename['filename'])
-    assert len(response['data']) == 1
-    assert type(response['data'][0]['processed_id']) is str
+    assert len(response_json['data']) == 1
+    assert type(response_json['data'][0]['processed_id']) is str
 
     global processed_id
-    processed_id = response['data'][0]['processed_id']
+    processed_id = response_json['data'][0]['processed_id']
+
+@pytest.mark.dependency(depends=['test_import_archived_code_system'])
+def test_retrieve_code_system_log():
+    time.sleep(5)
+
+    headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
+    encoded_uri = {
+        'processed_id': processed_id,
+    }
+
+    response = client.get(f'/api/v1/retrieve_code_system_log?{encoded_uri}', headers=headers)
+    response_json = response.json()
+
+    assert response_json['status'] == 200
+    assert response_json['message'] == 'Retrieve code system importing log is successful'
+    assert len(response_json['data']) == 1
