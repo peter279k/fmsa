6a1afc461a5655e16d0c6a03a98e857abc74de86
peter279k <peter279k@gmail.com>
2025-04-30 00:57:17 +0800

    Add CircuitBreakerMiddleware for FHIR Converter

--- fhir_converter/app/main.py
+++ fhir_converter/app/main.py
@@ -17,7 +17,6 @@ app = FastAPI(
     }
 )
 
-'''
 app.add_middleware(
     CircuitBreakerMiddleware,
     circuit_breaker_input=CircuitBreakerInputDto(
@@ -26,6 +25,5 @@ app.add_middleware(
         half_open_retry_timeout_seconds=30,
     ),
 )
-'''
 
 app.include_router(converter_router)
