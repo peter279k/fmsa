d4ff4d73d95eb4ac3d73e4f030e3ef0a9d17db30
peter279k <peter279k@gmail.com>
2025-04-29 20:52:54 +0800

    Add middleware for fhir_converter

--- fhir_converter/app/main.py
+++ fhir_converter/app/main.py
@@ -1,6 +1,7 @@
 from app.routers import *
 
 from fastapi import FastAPI
+from circuit_breaker import CircuitBreakerMiddleware, CircuitBreakerInputDto
 
 
 description = '''
@@ -14,6 +15,15 @@ app = FastAPI(
         'name': 'Peter',
         'email': 'peter279k@gmail.com',
     }
+)
+
+app.add_middleware(
+    CircuitBreakerMiddleware,
+    circuit_breaker_input=CircuitBreakerInputDto(
+        exception_list=(Exception,),
+        half_open_retry_count=3,
+        half_open_retry_timeout_seconds=30,
+    ),
 )
 
 app.include_router(converter_router)
