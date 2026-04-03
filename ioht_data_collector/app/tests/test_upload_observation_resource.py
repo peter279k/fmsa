45076880f6a84e0251c1ef2fe4a73ea8851491b9
peter279k <peter279k@gmail.com>
2026-04-04 01:43:08 +0800

    Add Organization resource updating during test

--- ioht_data_collector/app/tests/test_upload_observation_resource.py
+++ ioht_data_collector/app/tests/test_upload_observation_resource.py
@@ -9,6 +9,20 @@ client = TestClient(app)
 def test_upload_observation_resource_without_meta_profile_and_text():
     headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
 
+    with open('/app/app/tests/Organization-ltc-organization-example.json') as f:
+        json_str = f.read()
+
+    json_dict = json.loads(json_str)
+    del json_dict['meta']
+    del json_dict['text']
+
+    payload = {
+        'resource': json_dict,
+    }
+    response = httpx.put('http://fhir_data_manager:8000/api/v1/update/Organization', headers=headers, json=payload)
+
+    assert response.status_code == 201
+
     with open('/app/app/tests/Patient-ltc-patient-chen-ming-hui.json') as f:
         json_str = f.read()
 
