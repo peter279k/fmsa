c19aa31c8b986c33101d12ce358816a1dab23afb
peter279k <peter279k@gmail.com>
2026-04-08 23:44:52 +0800

    Test half-open state for LTC TW SC1

--- api_gateway/app/tests/rq/test_circuit_breaker_pattern_rq2.py
+++ api_gateway/app/tests/rq/test_circuit_breaker_pattern_rq2.py
@@ -210,21 +210,71 @@ def test_half_open_state_convert_upload_retrieve_observation_scenario1():
     del expected_json['text']
     del expected_json['id']
 
+    empty_payload = {}
+
     json_dict = {}
-    json_dict['payload'] = {}
+    json_dict['payload'] = empty_payload
+
+    for _ in range(6):
+        response = httpx.post('http://api_gateway:8000/api/v1/ltc_tw_2025_observation_blood_pressure', headers=headers, json=json_dict)
+
+        assert response.status_code == 500
+
+
+    payload = payload = {
+        'resourceType': 'Observation',
+        'profile_urls': ['http://ltc-ig.fhir.tw/StructureDefinition/LTCObservationVitalSigns'],
+        'status': 'final',
+        'category_coding': [{
+            'system': 'http://terminology.hl7.org/CodeSystem/observation-category',
+            'code': 'vital-signs',
+            'display': 'Vital Signs'
+        }],
+        'code_coding': [{
+            'system': 'http://loinc.org',
+            'code': '85354-9',
+            'display': 'Blood pressure panel with all children optional'
+        }],
+        'code_text': '血壓',
+        'subject': {
+            'reference': 'Patient/ltc-patient-chen-ming-hui'
+        },
+        'effective_datetime': '2024-01-15T09:00:00+08:00',
+        'performer': [{
+            'reference': 'PractitionerRole/ltc-practitioner-role-nurse-example'
+        }],
+        'component': [{
+            'code': {
+                'coding': [{
+                    'system': 'http://loinc.org',
+                    'code': '8480-6',
+                    'display': 'Systolic blood pressure'
+                }]
+            },
+            'valueQuantity': {
+                'value': 135,
+                'unit': 'mmHg',
+                'system': 'http://unitsofmeasure.org',
+                'code': 'mm[Hg]'
+            }
+        },
+        {
+            'code': {
+                'coding': [{
+                    'system': 'http://loinc.org',
+                    'code': '8462-4',
+                    'display': 'Diastolic blood pressure'
+                }]
+            },
+            'valueQuantity': {
+                'value': 85,
+                'unit': 'mmHg',
+                'system': 'http://unitsofmeasure.org',
+                'code': 'mm[Hg]'
+            }
+        }]
+    }
+
     response = httpx.post('http://api_gateway:8000/api/v1/ltc_tw_2025_observation_blood_pressure', headers=headers, json=json_dict)
-
-    response_json = response.json()
-    observation_id = response_json['data'][0]['id']
-    del response_json['data'][0]['id']
-
-    assert len(response_json['data'][0]['category']) == 1
-    assert len(response_json['data'][0]['code']['coding']) == 1
-
-    assert response.status_code == 200
-    assert len(response_json['data']) == 1
-    assert response_json['data'][0] == expected_json
-
-    response = httpx.get(f'http://api_gateway:8000/api/v1/retrieve/Observation?_id={observation_id}', headers=headers)
 
     assert response.status_code == 200
