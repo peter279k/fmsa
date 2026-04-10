6fbc301550d58e7595cedb0c13036bfe111e1e92
peter279k <peter279k@gmail.com>
2026-04-11 03:30:53 +0800

    Change run docker container and subprocess tasks

--- experiments/test_graceful_shutdown_rq1.py
+++ experiments/test_graceful_shutdown_rq1.py
@@ -6,12 +6,14 @@ from python_on_whales import DockerClient
 
 @pytest.mark.dependency()
 def test_send_signal_via_python_on_whales_can_be_graceful_shutdown():
+    docker = DockerClient(compose_files=['../docker-compose.yml'])
+
+    docker.compose.start(services=['fhir_generator'])
+
     subprocess.Popen([
         'python3', 'run_upload_location_data_background_task.py',
     ])
 
-    docker = DockerClient(compose_files=['../docker-compose.yml'])
-
-    docker.compose.start(services=['fhir_generator'])
     time.sleep(60)
+
     docker.compose.stop(services=['fhir_generator'], timeout=-1)
