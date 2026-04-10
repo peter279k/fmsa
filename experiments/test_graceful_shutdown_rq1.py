890ab8e4744c6b891e1ba0e83f059c89e18d9470
peter279k <peter279k@gmail.com>
2026-04-11 02:16:02 +0800

    Initial experiements for R1

---
+++ experiments/test_graceful_shutdown_rq1.py
@@ -0,0 +1,20 @@
+import time
+import pytest
+import subprocess
+from app.main import app
+from python_on_whales import DockerClient
+
+
+@pytest.fixture(scope='function')
+def run_background_task():
+    process = subprocess.Popen([
+        'python3', 'run_upload_location_data_background_task.py',
+    ])
+
+    yield process
+
+@pytest.mark.dependency()
+def test_send_signal_via_python_on_whales_can_be_graceful_shutdown():
+    time.sleep(5)
+    docker = DockerClient(compose_files=['../docker-compose.yml'])
+    docker.compose.stop(services=['fhir_generator'], timeout=-1)
